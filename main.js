3772a96ec69c84456aeb83f3cea2423583085e43
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-01-29 23:20:10 +1300

    tidy code

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+//set global variables
 var computerChoice;
+var userChoice;
+var winner;
+
+//assign computer choice to Math.random()
 var resetComputer = function(){
     computerChoice = Math.random();
     if (computerChoice < 0.34) {
@@ -10,41 +15,36 @@ var resetComputer = function(){
     } console.log("Computer: " + computerChoice);
 }
 
-var userChoice;
-var winner;
-
+//set start state
 var newGame = function(){
     $("#user_image").attr("src","images/fist.png");
-    $("#computer_image").attr("src","images/fist.png")
+    $("#computer_image").attr("src","images/fist.png");
     $("#user_controls").show();
     $("#result").hide();
-
 }
 
 var play = function(userChoice) {
-
-    resetComputer();
+    resetComputer(); //reassign random choice to computer
     compare(userChoice, computerChoice);
+    $("#user_image").attr("src", "images/" + userChoice + ".png"); //change img src according to userChoice
+    $("#computer_image").attr("src", "images/" + computerChoice + ".png"); //change img src according to computerChoice
 
-    $("#user_image").attr("src", "images/" + userChoice + ".png");
-    $("#computer_image").attr("src", "images/" + computerChoice + ".png");
-
-    document.getElementById("winner").innerHTML = winner;
+    document.getElementById("winner").innerHTML = winner;   //display var winner in winner p element
     
-    $("#user_controls").hide();
-    $("#result").show();
+    $("#user_controls").hide(); //hide control buttons
+    $("#result").show(); //show result div
 }
 
+//set winner and loser using userChoice and computerChoice
 var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
         winner = "It's a tie!"
     }
-    else if (choice1 === "rock") {
+    else if (choice1 === "rock") {  //if user chooses rock
         
-        if (choice2 === "scissors") {
-            winner = "You Win!"
-        }
-        else {
+        if (choice2 === "scissors") {   //and if computer chooses scissors
+            winner = "You Win!" //set var winner = you
+        } else {    //set winner = computer
             winner = "You Lose!"
         }
     }
@@ -52,8 +52,7 @@ var compare = function(choice1, choice2) {
         
         if (choice2 === "rock") {
             winner = "You Win!"
-        }
-        else {
+        } else {
             winner = "You Lose!"
         }
     }
@@ -61,8 +60,7 @@ var compare = function(choice1, choice2) {
         
         if (choice2 === "paper") {
             winner = "You Win!"
-        }
-        else {
+        } else {
             winner = "You Lose!" 
         }
     }
